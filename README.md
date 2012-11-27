4b884161e760040fe91a54449bfea23389793507
kevinschaul <kevin.schaul@gmail.com>
2012-11-26 18:40:36 -0600

    Prep for PyPI upload

--- README.md
+++ README.md
@@ -2,4 +2,13 @@ Open in GitHub
 
 A shortcut to your project on GitHub.com
 
+# Setup
+
+    pip install open-github
+
+# Usage
+
+    $ opengithub
+
+That simple.
 
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='opengithub',
+    version='0.1.0',
+    author='Kevin Schaul',
+    author_email='kevin.schaul@gmail.com',
+    url='http://www.kevinschaul.com',
+    description='Open your project in GitHub from the command line.',
+    long_description='Check out the project on GitHub for the latest information <http://github.com/kevinschaul/open-in-github>',
+    license='MIT',
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: Unix',
+    'Programming Language :: Python',
+    'Topci :: Software Development',
+    ],
+    packages=[
+    'opengithub',
+    ],
+    entry_points = {
+        'console_scripts': [
+            'opengithub = opengithub.opengithub:main',
+        ],
+    },
+)
+
