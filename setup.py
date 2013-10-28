8ff387b039a83d21eb4999d1bc1704a827b3c3a2
Kevin Schaul <kevin.schaul@gmail.com>
2013-10-27 21:53:36 -0500

    Increment version number, fix homepage

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='opengithub',
-    version='0.1.2',
+    version='0.1.3',
     author='Kevin Schaul',
     author_email='kevin.schaul@gmail.com',
-    url='http://www.kevinschaul.com',
+    url='http://kevin.schaul.io',
     description='Open your project in GitHub from the command line.',
     long_description='Check out the project on GitHub for the latest information <http://github.com/kevinschaul/open-in-github>',
     license='MIT',
