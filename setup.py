c5777561baa9db61e135d4313fa07ac2370520fc
Kevin Schaul <kevin.schaul@gmail.com>
2015-02-04 21:06:13 -0500

    Increment version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='opengithub',
-    version='0.3.1',
+    version='0.3.2',
     author='Kevin Schaul',
     author_email='kevin.schaul@gmail.com',
     url='http://kevin.schaul.io',
